71bf9e6541db83ddbe9f561ab475131d7fe85a06
unknown <jmcgirr@ECE-CPUIO24.NEXUS.UWATERLOO.CA>
2014-09-24 16:17:13 -0400

    Add NIOS project

---
+++ lab1part1/lab1part1/lab1part1_syslib/Debug/system_description/generated.gdb
@@ -0,0 +1,19 @@
+# generated.gdb
+#
+# Machine generated for a CPU named "cpu_0" as defined in:
+# N:\ece224\myESystem\ANT\my_controller.ptf
+#
+# Generated: 2014-09-24 16:07:40.857
+
+# DO NOT MODIFY THIS FILE
+#
+#   Changing this file will have subtle consequences
+#   which will almost certainly lead to a nonfunctioning
+#   system. If you do modify this file, be aware that your
+#   changes will be overwritten and lost when this file
+#   is generated again.
+#
+# DO NOT MODIFY THIS FILE
+
+# sdram_0
+mem 0x00800000 0x00800000+8388608 cache
---
+++ lab1part1/lab1part1/lab1part1_syslib/Debug/system_description/generated_app.mk
@@ -0,0 +1,37 @@
+# generated_app.mk
+#
+# Machine generated for a CPU named "cpu_0" as defined in:
+# N:\ece224\myESystem\ANT\my_controller.ptf
+#
+# Generated: 2014-09-24 16:07:26.442
+
+# DO NOT MODIFY THIS FILE
+#
+#   Changing this file will have subtle consequences
+#   which will almost certainly lead to a nonfunctioning
+#   system. If you do modify this file, be aware that your
+#   changes will be overwritten and lost when this file
+#   is generated again.
+#
+# DO NOT MODIFY THIS FILE
+
+# assuming the Quartus project directory is the same as the PTF directory
+QUARTUS_PROJECT_DIR = N:/ece224/myESystem/ANT
+
+# the simulation directory is a subdirectory of the PTF directory
+SIMDIR = $(QUARTUS_PROJECT_DIR)/my_controller_sim
+
+DBL_QUOTE := "
+
+
+
+verifysysid: dummy_verifysysid_file
+.PHONY: verifysysid
+
+dummy_verifysysid_file:
+	nios2-download $(JTAG_CABLE)                                --sidp=0x01001150 --id=0 --timestamp=1410966645
+.PHONY: dummy_verifysysid_file
+
+
+generated_app_clean:
+.PHONY: generated_app_clean
