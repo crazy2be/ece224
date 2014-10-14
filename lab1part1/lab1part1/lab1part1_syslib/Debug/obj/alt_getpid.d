addedf8671040d7d4d61f636f58572f7fbe231a3
Peter Raboud <pgraboud@uwaterloo.ca>
2014-10-14 18:39:17 -0400

    Remove some generated files from the repo

--- lab1part1/lab1part1/lab1part1_syslib/Debug/obj/alt_getpid.d
+++
@@ -1,3 +0,0 @@
-obj/alt_getpid.o:  \
- /cygdrive/c/Software/altera/10.1/nios2eds/components/altera_hal/HAL/src/alt_getpid.c \
-  /cygdrive/c/Software/altera/10.1/nios2eds/components/altera_hal/HAL/inc/os/alt_syscall.h
--- lab1part2/lab1part2_syslib_0/Debug/obj/alt_environ.d
+++
@@ -1,3 +0,0 @@
-obj/alt_environ.o:  \
- /cygdrive/c/Software/altera/10.1/nios2eds/components/altera_hal/HAL/src/alt_environ.c \
-  /cygdrive/c/Software/altera/10.1/nios2eds/components/altera_hal/HAL/inc/os/alt_syscall.h
